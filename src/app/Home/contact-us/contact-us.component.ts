c6772a8ccd745305765352d3993053f55a10f034
rahulpremani <34653669+rahulpremani@users.noreply.github.com>
2019-10-16 16:40:22 +0530

    fitness-tracking-app

---
+++ src/app/Home/contact-us/contact-us.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us',
+  template: `
+    <p>
+      contact-us works!
+    </p>
+  `,
+  styles: []
+})
+export class ContactUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Home/header/header.component';
+import { FooterComponent } from './Home/footer/footer.component';
+import { WelcomeComponent } from './Home/welcome/welcome.component';
+import { AboutUsComponent } from './Home/about-us/about-us.component';
+import { ContactUsComponent } from './Home/contact-us/contact-us.component';
+import { FeaturesComponent } from './Home/features/features.component';
+import { LoginComponent } from './Home/login/login.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    WelcomeComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    FeaturesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
