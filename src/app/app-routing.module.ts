930d20d1fb455a70f2033a78fa0a4d063cf30f62
ajaykshukla13 <ajaykshukla13@gmail.com>
2019-10-23 17:00:59 +0530

    made changes on 23 october

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './Home/welcome/welcome.component';
 import { LoginComponent } from './Home/login/login.component';
+import { AboutUsComponent } from './Home/about-us/about-us.component';
 
 const routes: Routes = [
   {path : '', component : WelcomeComponent},
-  {path : 'login', component : LoginComponent}
+  {path : 'login', component : LoginComponent},
+  {path : 'aboutus', component : AboutUsComponent}
 ];
 
 @NgModule({
