93a6fb6ab6b7e74ba702fb8d9659105703185405
rahulpremani <34653669+rahulpremani@users.noreply.github.com>
2019-11-08 09:15:15 +0530

    Updated Login

--- src/app/Home/login/login.component.ts
+++ src/app/Home/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 import { phoneNumberValidator } from 'src/app/validators/phone-validator';
 import { MustMatch } from 'src/app/_helpers/must-match.validator';
-
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-login',
@@ -13,11 +13,12 @@ export class LoginComponent implements OnInit {
   
   emailPattern: string = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
   passwordPattern: string = "^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$";
-  namePattern: string = "[a-zA-Z]*";
+  namePattern: string = "[a-zA-Z\\s]*$";
   agePattern: string = "^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$";
- 
   
-  
+  loginForm: FormGroup;
+  signupForm: FormGroup;
+
   get getLoginEmail(){
     return this.loginForm.get('email');
   }
@@ -50,10 +51,6 @@ export class LoginComponent implements OnInit {
     return this.signupForm.get('gender');
   }
 
-  get getAge(){
-    return this.signupForm.get('age');
-  }
-
   get getHeight(){
     return this.signupForm.get('height');
   }
@@ -68,30 +65,31 @@ export class LoginComponent implements OnInit {
   constructor(private lf: FormBuilder, private sf: FormBuilder) { }
   
   ngOnInit() {
-    document.querySelector('.img__btn').addEventListener('click', function() {
-      document.querySelector('.cont').classList.toggle('s--signup');
+    $('.message a').click(function(){
+      $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+   });
+
+    this.loginForm = this.lf.group({
+      email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+      password: ['', Validators.required]
     });
+    
+    this.signupForm = this.sf.group({
+      fullname: ['', [Validators.required, Validators.pattern(this.namePattern)]],
+      email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+      mobile: ['', [Validators.required, phoneNumberValidator, Validators.minLength(10)]],
+      password: ['', [Validators.required, Validators.pattern(this.passwordPattern)]],
+      gender: ['', Validators.required],
+      confirmpassword: ['', Validators.required],
+      height: ['', [Validators.required]],
+      weight: ['', [Validators.required, phoneNumberValidator]],
+      dateofbirth: ['', Validators.required]
+    }
+    ,{
+      validator: MustMatch('password', 'confirmpassword')
+    }
+    );
   }
-  
-  loginForm = this.lf.group({
-    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
-    password: ['', Validators.required]
-  });
-  
-  signupForm = this.sf.group({
-    fullname: ['', [Validators.required, Validators.pattern(this.namePattern)]],
-    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
-    mobile: ['', [Validators.required, phoneNumberValidator, Validators.minLength(10)]],
-    password: ['', [Validators.required, Validators.pattern(this.passwordPattern)]],
-    age: ['', Validators.required, Validators.pattern(this.agePattern)],
-    gender: ['', Validators.required],
-    confirmpassword: ['', Validators.required],
-    height: ['', [Validators.required, phoneNumberValidator]],
-    weight: ['', [Validators.required, phoneNumberValidator]],
-    dateofbirth: ['', Validators.required]
-  },{
-    validator: MustMatch('password', 'confirmpassword')
-  });
 
 }
 
